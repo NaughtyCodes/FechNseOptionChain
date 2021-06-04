7a1ff1d46dc7d1295d6076d307cb537b2b048460
naughtycodes <krish_nan@outlook.com>
2021-06-04 18:48:19 +0530

    added swagger configuration

--- src/main/java/com/naughtycodes/lab/options/app/config/Swagger2UiConfig.java
+++ src/main/java/com/naughtycodes/lab/options/app/config/Swagger2UiConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 import com.google.common.base.Predicates;
 
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -25,8 +26,8 @@ public class Swagger2UiConfig extends WebMvcConfigurerAdapter {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 				// .apis(RequestHandlerSelectors.any())
 				.apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
-				// .paths(PathSelectors.any())
-				// .paths(PathSelectors.ant("/swagger2-demo"))
+				//.paths(PathSelectors.any())
+				//.paths(PathSelectors.ant("/api"))
 				.build();
 		// @formatter:on
 	}
