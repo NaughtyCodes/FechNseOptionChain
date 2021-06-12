0cd2bee184b9240d1b12dd80ef6a07e54b449853
naughtycodes <krish_nan@outlook.com>
2021-06-12 22:20:58 +0530

    clean up

--- src/test/java/com/naughtycodes/lab/options/app/URLEncodingTest.java
+++ src/test/java/com/naughtycodes/lab/options/app/URLEncodingTest.java
@@ -1,10 +1,12 @@
 package com.naughtycodes.lab.options.app;
 
+import java.net.URLEncoder;
+
 public class URLEncodingTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		URLEncoder.encode("https://www1.nseindia.com/marketinfo/companyTracker/mtOptionKeys.jsp?companySymbol=L%26TFH&indexSymbol=NIFTY&series=EQ&instrument=OPTSTK&date=29JUL2021");
 	}
 
 }
